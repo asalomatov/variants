6fad0509541d5d2cd20dc6a158dabea4d1e675c9
Andrei Salomatov <andrei.salomatov@gmail.com>
2016-04-13 11:17:49 -0400

    clean up

--- variants/prin_dbsnpDescr.py
+++
@@ -1,8 +0,0 @@
-import variants
-
-
-
-### dbSNP fields
-myfile = '/mnt/xfs1/bioinfo/data/bcbio_nextgen/150607/genomes/Hsapiens/GRCh37/variation/dbsnp_138.vcf.gz'
-myvars = variants.Variants(myfile, 'aaa')
-myvars.describeInfoFields()
