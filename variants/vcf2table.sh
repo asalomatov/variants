c482442b056ab7218adb7a5a60543aefcf02c9a6
Andrei Salomatov <andrei.salomatov@gmail.com>
2017-05-16 11:27:50 -0400

    switched order between vep and snpeff

--- variants/vcf2table.sh
+++ variants/vcf2table.sh
@@ -11,11 +11,11 @@ work_dir="$(dirname $input_tsv)"
 cp ${script_dir}/header_extra.txt ${work_dir}/tmp-${prefix}.vcf
 cat $input_tsv | sort -V -k1,1 -k2,2 >> ${work_dir}/tmp-${prefix}.vcf
 vcfintersect -b $target_bed ${work_dir}/tmp-${prefix}.vcf > ${work_dir}/${prefix}.vcf
+echo 'running VEP'
+make -f ${script_dir}/annVEP.mk INCLMK=$incl_make PREFIX=${prefix} SUFFIX=.vcf INDIR=$work_dir OUTDIR=$work_dir
 echo 'running snpEff'
 make -f ${script_dir}/annSnpEff.mk INCLMK=$incl_make PREFIX=${prefix} SUFFIX=.vcf INDIR=$work_dir OUTDIR=$work_dir
 cat ${work_dir}/${prefix}-ann.vcf | \
     /mnt/xfs1/bioinfo/software/installs/bcbio_nextgen/150607/Cellar/snpeff/4.1g/share/scripts/vcfEffOnePerLine.pl > \
     ${work_dir}/${prefix}-ann-onePline.vcf
 ${script_dir}/extractFields.sh ${work_dir}/${prefix}-ann-onePline.vcf > ${work_dir}/${prefix}-ann-onePline.tsv
-echo 'running VEP'
-make -f ${script_dir}/annVEP.mk INCLMK=$incl_make PREFIX=${prefix} SUFFIX=.vcf INDIR=$work_dir OUTDIR=$work_dir
